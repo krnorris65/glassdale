c5149ed9c231d20aac216c917223b237ba793a02
Kristen Norris <krnorris65@gmail.com>
2020-08-03 11:38:38 -0500

    show multiple facilities for criminals

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,14 +1,23 @@
-export const Criminal = (criminalObject) => {
+export const Criminal = (criminalObject, facilities) => {
     return `
     <div class="criminal">
         <h4>${criminalObject.name}</h4>
         <div class="criminal__details">
+            <p>Convicted for ${criminalObject.conviction}</p>
+            <p>Arrested by ${criminalObject.arrestingOfficer}</p>
+            <p>Incarcerated between:
+                ${new Date(criminalObject.incarceration.start).toLocaleDateString()} and
+                ${new Date(criminalObject.incarceration.end).toLocaleDateString()}
+            </p>
             <p>Age: ${criminalObject.age}</p>
-            <p>Crime: ${criminalObject.conviction}</p>
-            <p>Term start: ${new Date(criminalObject.incarceration.start).toLocaleDateString()}</p>
-            <p>Term end: ${new Date(criminalObject.incarceration.end).toLocaleDateString()}</p>
+            <div>
+                <h2>Facilities</h2>
+                <ul>
+                    ${facilities.map(f => `<li>${f.facilityName}</li>`).join("")}
+                </ul>
+            </div>
+            <button id="associates--${criminalObject.id}">Show Associates</button>
         </div>
-        <button id="associates--${criminalObject.id}">Associate Alibis</button>
     </div>
     `
 }
--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -3,9 +3,11 @@ import { useCriminals, getCriminals } from "./CriminalProvider.js"
 import { useConvictions } from "../convictions/ConvictionProvider.js";
 import { useOfficers } from "../officers/OfficerProvider.js";
 import { hideOtherListContainers } from "../helpers/hideElement.js";
+import { getCriminalFacilities, useCriminalFacilities } from "../facility/CriminalFacilityProvider.js";
+import { getFacilities, useFacilities } from "../facility/FacilityProvider.js";
 
 const eventHub = document.querySelector(".container")
-const criminalContainer = document.querySelector('.criminalsContainer')
+const contentTarget = document.querySelector('.criminalsContainer')
 
 
 eventHub.addEventListener('chosenCrime', event => {
@@ -17,7 +19,7 @@ eventHub.addEventListener('chosenCrime', event => {
         /*
             Filter the criminals application state down to the people that committed the crime
         */
-       const selectedCrime = crimeCollection.find(crime => crime.id === Number(event.detail.crimeId))
+        const selectedCrime = crimeCollection.find(crime => crime.id === Number(event.detail.crimeId))
 
         const matchingCriminals = appStateCriminals.filter(criminal => criminal.conviction === selectedCrime.name)
 
@@ -36,12 +38,12 @@ eventHub.addEventListener('chosenOfficer', event => {
     const appStateCriminals = useCriminals()
     const officerCollection = useOfficers()
 
-    if("officerId" in event.detail && event.detail.officerId !== "0"){
+    if ("officerId" in event.detail && event.detail.officerId !== "0") {
 
         const selectedOfficer = officerCollection.find(officer => officer.id === Number(event.detail.officerId))
 
         const matchingCriminals = appStateCriminals.filter(criminal => criminal.arrestingOfficer === selectedOfficer.name)
-        
+
         render(matchingCriminals)
     } else {
         // if an officer isn't selected then return all the criminals
@@ -49,23 +51,39 @@ eventHub.addEventListener('chosenOfficer', event => {
     }
 })
 
-const render = criminalCollection => {
-    hideOtherListContainers(criminalContainer)
-    let criminalHtmlRepresentation = ""
-    criminalCollection.forEach(criminal => {
-        criminalHtmlRepresentation += Criminal(criminal)
-    })
-    criminalContainer.innerHTML = criminalHtmlRepresentation
+const render = (criminalsToRender, allFacilities, allRelationships) => {
+    hideOtherListContainers(contentTarget)
+    // Step 1 - Iterate all criminals
+    contentTarget.innerHTML = criminalsToRender.map(
+        (criminalObject) => {
+            // Step 2 - Filter all relationships to get only ones for this criminal
+            const facilityRelationshipsForThisCriminal = allRelationships.filter(cf => cf.criminalId === criminalObject.id)
+
+            // Step 3 - Convert the relationships to facilities with map()
+            const facilities = facilityRelationshipsForThisCriminal.map(cf => {
+                const matchingFacilityObject = allFacilities.find(facility => facility.id === cf.facilityId)
+                return matchingFacilityObject
+            })
+
+            // Must pass the matching facilities to the Criminal component
+            return Criminal(criminalObject, facilities)
+        }
+    ).join("")
 }
 
 
 
-// Render ALL criminals initally
+// Render ALL criminals initially
 export const CriminalList = () => {
-    getCriminals().then(() => {
-        const appStateCriminals = useCriminals()
-        render(appStateCriminals)
-    })
+    getCriminals()
+        .then(getFacilities)
+        .then(getCriminalFacilities)
+        .then(() => {
+            const appStateCriminals = useCriminals()
+            const allFacilities = useFacilities()
+            const allCriminalFacilities = useCriminalFacilities()
+            render(appStateCriminals, allFacilities, allCriminalFacilities)
+        })
 }
 
 
