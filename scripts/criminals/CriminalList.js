c48cf16fbbdc0f4f5f8b18480f8a21c2b3c5abb8
Kristen Norris <krnorris65@gmail.com>
2020-08-03 11:42:10 -0500

    fixed bug

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -13,7 +13,8 @@ const contentTarget = document.querySelector('.criminalsContainer')
 eventHub.addEventListener('chosenCrime', event => {
     const appStateCriminals = useCriminals()
     const crimeCollection = useConvictions()
-
+    const allFacilities = useFacilities()
+    const allCriminalFacilities = useCriminalFacilities()
     // You remembered to add the id of the crime to the event detail, right?
     if ("crimeId" in event.detail && event.detail.crimeId !== "0") {
         /*
@@ -27,27 +28,28 @@ eventHub.addEventListener('chosenCrime', event => {
             Then invoke render() and pass the filtered collection as
             an argument
         */
-        render(matchingCriminals)
+        render(matchingCriminals, allFacilities, allCriminalFacilities)
     } else {
         // if a crime isn't selected then return all the criminals
-        render(appStateCriminals)
+        render(appStateCriminals, allFacilities, allCriminalFacilities)
     }
 })
 
 eventHub.addEventListener('chosenOfficer', event => {
     const appStateCriminals = useCriminals()
     const officerCollection = useOfficers()
-
+    const allFacilities = useFacilities()
+    const allCriminalFacilities = useCriminalFacilities()
     if ("officerId" in event.detail && event.detail.officerId !== "0") {
 
         const selectedOfficer = officerCollection.find(officer => officer.id === Number(event.detail.officerId))
 
         const matchingCriminals = appStateCriminals.filter(criminal => criminal.arrestingOfficer === selectedOfficer.name)
 
-        render(matchingCriminals)
+        render(matchingCriminals, allFacilities, allCriminalFacilities)
     } else {
         // if an officer isn't selected then return all the criminals
-        render(appStateCriminals)
+        render(appStateCriminals, allFacilities, allCriminalFacilities)
     }
 })
 
